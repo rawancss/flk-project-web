1f4734ce05f74c72ee8b581201bf586bb44e6f31
rawancss <151460963+rawancss@users.noreply.github.com>
2024-03-18 16:34:43 -0700

    add files

---
+++ webproject-final/project/project/js/script.js
@@ -0,0 +1,40 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+
+// Remove item from cart and update the count of items in cart
+function removeItem(e){
+    e.parentNode.remove();
+
+    let cartItem = document.querySelector('.cart-items-container');
+    let cartItemText = document.querySelector('.notification-count');
+    cartItemText.innerHTML = cartItem.querySelectorAll('.cart-item').length;
+
+}
